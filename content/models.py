b2029f9729b0c2b4c5decb787148616bed556793
melihakan <melih_akan_35@hotmail.com>
2021-05-08 15:37:22 +0300

    Category and Content Relation

--- content/models.py
+++ content/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 class Category(models.Model):
     STATUS = (
         ('True', 'True'),
@@ -17,3 +18,23 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+class Content(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE) #many to one relation with Category
+    title = models.CharField(max_length=150)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image=models.ImageField(blank=True,upload_to='images/')
+
+    file = models.FileField(blank=True, upload_to='files/')
+
+    detail=models.TextField()
+    status=models.CharField(max_length=10,choices=STATUS)
+    create_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
