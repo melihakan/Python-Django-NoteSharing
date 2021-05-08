8df92c2e17accbdd9ce4d92ca042237be15ad193
melihakan <melih_akan_35@hotmail.com>
2021-05-08 14:21:42 +0300

    Category created

--- note/urls.py
+++ note/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+    path('content/', include('content.urls')),
     path('home/', include('home.urls')),
 ]
